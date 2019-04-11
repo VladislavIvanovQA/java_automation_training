c1cd757ecf9506654fbbdf15d460724ced538487
v.ivanov <ivanovvlad.261996@mail.ru>
2019-04-11 19:52:45 +0300

    Change signature. Add module for Api tests.

--- src/main/java/com/training/Methods.java
+++
@@ -1,9 +0,0 @@
-package com.training;
-
-public abstract class Methods {
-
-    public abstract int sum(int a, int b);
-    public abstract int val(int a, int b);
-    public abstract int minus(int a, int b);
-    public abstract int del(int a, int b);
-}
